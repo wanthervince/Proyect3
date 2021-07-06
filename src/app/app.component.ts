c706cb707e0a09b2a598f1ebe1e62101d64ea763
WanTher-VinCe <lonely.wolf.1594@gmail.com>
2021-07-06 16:36:37 -0500

    proyecto piano terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'proyect3';
+  sound(n: number): void {
+    const sound = new Audio();
+    sound.src = `../assets/sound/note${n}.wav`;
+    sound.load();
+    sound.play();
+  }
 }
